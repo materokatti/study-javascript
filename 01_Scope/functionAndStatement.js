07edac8d771c1c08baeb0be4f7b24a064be6ea2c
Doyun_hwang <materokatti@gmail.com>
2024-01-16 17:30:27 +0900

    functionAndStatement.js : function and statement in scope

---
+++ 01_Scope/functionAndStatement.js
@@ -0,0 +1,21 @@
+var var1 = 1;
+
+if (1) {
+  var var2 = 2;
+  if (true) {
+    var var3 = 3;
+  }
+}
+
+function hey() {
+  var var4 = 4;
+  if (true) {
+    var var5 = 5;
+  }
+}
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(var4);
+console.log(var5);
