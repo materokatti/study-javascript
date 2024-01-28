34660eabeee5298212093b26a01386dd458b0072
Doyun <materokatti@gmail.com>
2024-01-29 08:48:40 +0900

    whyUseOOP.ts

---
+++ 05_TS_OOP/whyUseOOP.ts
@@ -0,0 +1,12 @@
+const message1 = { title: undefined, message: undefined };
+const message2 = { title: undefined, message: undefined };
+const message3 = { title: undefined, message: undefined };
+
+export class Message {
+  title;
+  message;
+  // id (you can add an 'id' for every variable.)
+}
+
+const message4 = new Message();
+const message5 = new Message();
